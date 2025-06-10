8151b6f08bbffdcb47fd49185f39f4ac27330532
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-10 12:30:28 +0500

    menu-btn is added in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import Button from './Button'
 import Logo from "../assets/logos/logo-dark-transparent.png"
-
+import { Text, X } from "lucide-react";
 
 function NavBar() {
 
     
     return (
 
-        <nav className="w-full h-[100px] bg-white shadow-md shadow-[#dad] flex items-center justify-between px-20 " >
-             <div className="size-40 flex items-center justify-center">
-                <img src={Logo}  alt="ogo" />
+        <nav className="w-full h-[100px] bg-white shadow-md shadow-[#dad] flex items-center justify-between 2xl:px-24 xl:px-3 lg:px-16 max-md:px-6   " >
+             <div className="flex h-full w-[160px] items-center justify-center">
+                <img src={Logo} className=" w-[160px] object-cover " alt="ogo" />
              </div>
             
-            <ul className="flex gap-10 text-[25px] uppercase font-semibold list-none  " >
-                <li className='hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Home</a> </li>
-                <li className='hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > About</a> </li>
-                <li className='hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Portfolio</a> </li>
-                <li className='hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Services</a> </li>
-                <li className='hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Experience</a> </li>
+            <ul className=" hidden xl:flex gap-10 text-[25px]  uppercase font-semibold list-none  " >
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Home</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > About</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Portfolio</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Services</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Experience</a> </li>
+                <li className='xl:ml-20'><Button text="Contact"/></li>
             </ul>
- 
-            <div>
-                <Button text="Contact" />
-            </div>
+  
+             <button className='bg-white block xl:hidden rounded-[8px]' >
+                <Text size={48} color='#a80b28' strokeWidth={3} />
+             </button>
+           
         </nav>
     )
 }
