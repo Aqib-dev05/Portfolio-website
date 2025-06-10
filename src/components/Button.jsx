69ad03fd08a6f5fc732dbeb8e0253499b42931b7
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-10 12:53:49 +0500

    navbar is completed.Final

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,8 @@ export default function Button({text,className="",varient="default",onClick}) {
     const baseStyles = "font-semibold text-[20px] w-fit h-[50px] font-medium px-6 rounded-[6px] cursor-pointer border transition-all duration-300 ";
     const varientStyles = {
         default: " bg-gradient-to-b  from-[#a80a27] to-[#6a039e] text-white  border-none hover:bg-gradient-to-r",
-        outlined: "bg-white text-black border-none shadow-lg hover:bg-gradient-to-r from-[crimson] to-[purple] hover:text-white hover:border-none",
+        outlined: "bg-transparent text-white border-white hover:bg-white hover:text-black transition-all duration-400",
+        shadowed: "bg-white text-black border-none shadow-lg hover:bg-gradient-to-r from-[crimson] to-[purple] hover:text-white hover:border-none",
     };
 
     return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Button from './Button'
 import Logo from "../assets/logos/logo-dark-transparent.png"
-import { Text, X } from "lucide-react";
+import { Text } from "lucide-react";
+import { useState } from 'react';
 
 function NavBar() {
 
+    const [isOpen, setIsOpen] = useState(false);
+    const handleOpen = () => {
+        setIsOpen(!isOpen);
+    };
+    // const handleClose = () => {
+    //     setIsOpen(false);
+    // }
     
     return (
-
-        <nav className="w-full h-[100px] bg-white shadow-md shadow-[#dad] flex items-center justify-between 2xl:px-24 xl:px-3 lg:px-16 max-md:px-6   " >
+            <>
+        <nav className="w-full h-[100px] bg-white shadow-md shadow-[#dad] flex items-center justify-between 2xl:px-24 xl:px-3 lg:px-16 max-md:px-4 max-lg:px-20  " >
              <div className="flex h-full w-[160px] items-center justify-center">
                 <img src={Logo} className=" w-[160px] object-cover " alt="ogo" />
              </div>
@@ -22,11 +30,30 @@ function NavBar() {
                 <li className='xl:ml-20'><Button text="Contact"/></li>
             </ul>
   
-             <button className='bg-white block xl:hidden rounded-[8px]' >
+             <button onClick={handleOpen} className='bg-white block xl:hidden rounded-[8px]' >
                 <Text size={48} color='#a80b28' strokeWidth={3} />
              </button>
            
         </nav>
+
+         {isOpen && (
+        <div className="relative top-0 left-0 max-w-[100vw] h-[100vh] bg-[#2c0a10]  text-white  z-50">
+          <div className=" h-full p-5 ">
+            {/* <button onClick={handleClose} className="text-black absolute z-50 top-2 right-3 text-2xl">
+              <X color="#ffff" size={48} />
+            </button> */}
+            <ul className="flex flex-col items-center justify-center mt-16 gap-4 text-[22px] uppercase font-medium list-none  " >
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Home</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > About</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Portfolio</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Services</a> </li>
+                <li className='mt-1 hover:text-[#a80a27] transition-all duration-300 ' ><a href='#' > Experience</a> </li>
+                <li className='xl:ml-20'><Button varient='outlined' text="Contact"/></li>
+            </ul>
+          </div>
+        </div>
+      )}
+        </>
     )
 }
 
