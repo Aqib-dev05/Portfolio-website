a99d00b7239dbe3da2e35a0b91e02c979e8faeac
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-10 11:01:31 +0500

    button component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Button from "./components/Button";
+
 export default function App(){
 
   return(
     <>
-   
+     <Button text="Follow Me"  />
+     <Button text="Contact me" varient="outlined" />
+
     </>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Button({text,varient="default"}) {
+
+    const baseStyles = "font-semibold text-[20px] w-fit h-[50px] font-medium px-6 rounded-[6px] cursor-pointer border transition-all duration-300 ";
+    const varientStyles = {
+        default: " bg-gradient-to-b  from-[#a80a27] to-[#6a039e] text-white  border-none hover:bg-gradient-to-r",
+        outlined: "bg-white text-black border-none shadow-lg hover:bg-gradient-to-r from-[crimson] to-[purple] hover:text-white hover:border-none",
+    };
+
+    return (
+        <button className={`${baseStyles} ${varientStyles[varient]}   `}  >{text}</button>
+    )
+}
