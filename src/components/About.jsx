2ef663a61e3ef159aa8dca6644b97043791da339
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-11 16:18:56 +0500

    z

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -34,23 +34,23 @@ function About() {
         </div>
       </div>
       <div className="w-full max-lg:pt-22 max-lg:text-center lg:w-[50%] lg:h-[100%] flex flex-col p-2 lg:p-7 ">
-        <h2 className="  my-3 text-6xl font-bold font-[cursive] text-[#a80a27] ">
+        <h2 className="blen  my-3 text-6xl font-bold font-[cursive] bg-gradient-to-l  from-[#a80a27] to-[#8c0eca] text-transparent w-fit ">
           About Me
         </h2>
         <h3 className="mt-6 text-7xl max-lg:text-4xl max-xl:text-5xl font-bold ">
           1.5 Year's Experience in Web Development{" "}
         </h3>
-        <p className="mt-6 text-xl  font-medium text-gray-800">
+        <p className="mt-6 text-xl w-[95%] font-medium text-gray-800">
           I'm Aqib ALi, a frontend developer passionate about creating clean,
           responsive, and engaging web experiences. I specialize in React,
           JavaScript, HTML, and CSS, and I’m currently leveling up with Tailwind
           CSS, Jest, and API integrations.
-          <br />
+          <br /><br />
           I enjoy building UIs that not only look good but work smoothly and
           efficiently. Beyond development, I’m also exploring SEO and how
           websites can help businesses grow online. I’m always eager to learn,
           take on new challenges, and bring creative ideas to life through code.
-          <br />
+          <br /><br />
           Let’s create something impactful together!
         </p>
         <div className="flex gap-4 mt-8 w-full max-lg:justify-center">
