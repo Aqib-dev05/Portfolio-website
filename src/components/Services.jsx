a1c9710c37ca204fdbcbfdbc8345a2ba204a68c0
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-11 20:42:54 +0500

    mobile view is sett of service cards

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -39,15 +39,15 @@ function Services() {
     <section className='w-full py-10'>
         <h2 className='text-center py-4 text-5xl border-b-4 border-[#a80a27] w-fit mx-auto font-extrabold'>Our <span className='bg-gradient-to-b  from-[#a80a27] to-[#6a039e] blen text-transparent' >Services</span></h2>
         <p className='lg:w-[40%] w-[86%] text-center mx-auto my-5 text-lg'>We deliver high-quality digital solutions crafted to meet your specific needs and exceed expectations.</p>
-        <div className="w-full xl:w-[85%] mx-auto p-6  rounded-lg  flex flex-wrap justify-center items-center gap-8">
+        <div className="w-full xl:w-[85%] mx-auto md:p-6 p-2  rounded-lg  flex flex-wrap justify-center items-center gap-8">
             {services.map((service) => (
-                <div key={service.id} className="flex w-[250px] h-[450px] bg-[#a80a272a] rounded-2xl min-h-[300px] flex-col items-start gap-4 p-6 border-b border-gray-200">
+                <div key={service.id} className="flex w-[310px] h-[450px] bg-[#a80a272a] rounded-2xl min-h-[300px] flex-col items-start gap-4 p-6 border-b border-gray-200">
                     <div className="flex items-center justify-center w-11 h-11 bg-[#a80a27] rounded-full">
                         {service.icon}
                     </div>
                     <div>
                         <h3 className="text-[23px] my-4 font-semibold text-gray-800">{service.title}</h3>
-                        <p className="text-gray-600 mt-8 text-[16px] ">{service.description}</p>
+                        <p className="text-gray-600 mt-8 text-[18px] ">{service.description}</p>
                     </div>
                     <a href="#" className='flex items-center justify-center text-lg text-[#a80a27] font-medium '>Learn More <ChevronRight/> </a>
                 </div>
